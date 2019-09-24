0a545443218fcda2c0f2887917b1a61b7001fa86
Jinyoung <40735154+diligejy@users.noreply.github.com>
2019-09-24 19:19:34 +0900

    Add files via upload
    한국거래소 오늘의 공시 크롤링 완료(Post)

---
+++ R_finance/R_Quant_Portfolio/today_announcement.R
@@ -0,0 +1,27 @@
+
+library(httr)
+library(rvest)
+
+Sys.setlocale("LC_ALL", "English")
+
+url = 'http://kind.krx.co.kr/disclosure/todaydisclosure.do'
+data = POST(url, body = 
+              list(
+                method = 'searchTodayDisclosureSub',
+                currentPageSize = '15',
+                pageIndex = '1',
+                orderMode = '0',
+                orderStat = 'D',
+                forward = 'todaydisclosure_sub',
+                chose = 'S',
+                todayFlag = 'Y',
+                selDate = '2018-12-28'
+              ))
+
+data = read_html(data) %>%
+  html_table(fill = TRUE) %>%
+  .[[1]]
+
+Sys.setlocale("LC_ALL", "Korean")
+
+print(head(data))
