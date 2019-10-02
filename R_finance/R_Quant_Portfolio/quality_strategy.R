24704a6e8ee1910c5644a03fcc2a614f5af066d1
Jinyoung <40735154+diligejy@users.noreply.github.com>
2019-10-02 13:37:27 +0900

    Add files via upload

---
+++ R_finance/R_Quant_Portfolio/quality_strategy.R
@@ -0,0 +1,106 @@
+
+library(stringr)
+library(ggplot2)
+library(dplyr)
+
+KOR_fs = readRDS('C:/Users/jinyoung/Desktop/R_finance/R_Quant_Portfolio/data/KOR_fs.Rds')
+KOR_ticker = read.csv('C:/Users/jinyoung/Desktop/R_finance/R_Quant_Portfolio/data/KOR_ticker.csv', row.names = 1,
+                      stringsAsFactors = FALSE)
+KOR_ticker$'종목코드' = 
+  str_pad(KOR_ticker$'종목코드', 6, 'left', 0)
+
+# 수익성
+ROA = KOR_fs$'지배주주순이익' / KOR_fs$'자산'
+CFO = KOR_fs$'영업활동으로인한현금흐름' / KOR_fs$'자산'
+ACCURUAL = CFO - ROA
+
+# 재무성과
+LEV = KOR_fs$'장기차입금' / KOR_fs$'자산'
+LIQ = KOR_fs$'유동자산' / KOR_fs$'유동부채'
+OFFER = KOR_fs$'유상증자'
+
+# 운영 효율성
+MARGIN = KOR_fs$'매출총이익' / KOR_fs$'매출액'
+TURN = KOR_fs$'매출액' / KOR_fs$'자산'
+
+num_col = ncol(KOR_fs[[1]])
+
+F_1 = as.integer(ROA[, num_col] > 0)
+F_2 = as.integer(CFO[, num_col] > 0)
+F_3 = as.integer(ROA[, num_col] - ROA[, (num_col-1)] > 0)
+F_4 = as.integer(ACCURUAL[, num_col] > 0) 
+F_5 = as.integer(LEV[, num_col] - LEV[, (num_col-1)] <= 0) 
+F_6 = as.integer(LIQ[, num_col] - LIQ[, (num_col-1)] > 0)
+F_7 = as.integer(is.na(OFFER[,num_col]) |
+                   OFFER[,num_col] <= 0)
+F_8 = as.integer(MARGIN[, num_col] -
+                   MARGIN[, (num_col-1)] > 0)
+F_9 = as.integer(TURN[,num_col] - TURN[,(num_col-1)] > 0)
+F_Table = cbind(F_1, F_2, F_3, F_4, F_5, F_6, F_7, F_8, F_9) 
+F_Score = F_Table %>%
+  apply(., 1, sum, na.rm = TRUE) %>%
+  setNames(KOR_ticker$'종목명')
+
+(F_dist = prop.table(table(F_Score)) %>% round(3))
+
+F_dist %>%
+  data.frame() %>%
+  ggplot(aes(x = F_Score, y = Freq,
+             label = paste0(Freq * 100, '%'))) +
+  geom_bar(stat = 'identity') +
+  geom_text(color = 'black', size = 3, vjust = -0.4) +
+  scale_y_continuous(expand = c(0, 0, 0, 0.05),
+                     labels = scales ::percent) + 
+  ylab(NULL) +
+  theme_classic()
+
+invest_F_Score = F_Score %in% c(9)
+KOR_ticker[invest_F_Score, ] %>%
+  select(`종목코드`, `종목명`) %>%
+  mutate(`F-Score` = F_Score[invest_F_Score])
+
+
+# 각 지표를 결합하기
+
+library(stringr)
+library(ggplot2)
+library(dplyr)
+library(tidyr)
+
+KOR_fs = readRDS('C:/Users/jinyoung/Desktop/R_finance/R_Quant_Portfolio/data/KOR_fs.Rds')
+KOR_ticke = read.csv('C:/Users/jinyoung/Desktop/R_finance/R_Quant_Portfolio/data/KOR_ticker.csv', row.names = 1,
+                     stringsAsFactors = FALSE)
+
+KOR_ticker$'종목코드' = 
+  str_pad(KOR_ticker$'종목코드', 6, 'left', 0)
+
+num_col = ncol(KOR_fs[[1]])
+quality_roe = (KOR_fs$'지배주주순이익' / KOR_fs$'자본')[num_col]
+quality_gpa = (KOR_fs$'매출총이익' / KOR_fs$'자산')[num_col]
+quality_cfo = 
+  (KOR_fs$'영업활동으로인한현금흐름' / KOR_fs$'자산')[num_col]
+
+quality_profit = 
+  cbind(quality_roe, quality_gpa, quality_cfo) %>%
+  setNames(., c('ROE', 'GPA', 'CFO'))
+
+rank_quality = quality_profit %>% 
+  mutate_all(list(~min_rank(desc(.))))
+
+cor(rank_quality, use = 'complete.obs') %>%
+  round(., 2) %>%
+  corrplot(method = 'color', type = 'upper',
+           addCoef.col = 'black', number.cex = 1,
+           tl.cex = 0.6, tl.srt = 45, tl.col = 'black',
+           col =
+             colorRampPalette(c('blue', 'white', 'red'))(200),
+           mar=c(0,0,0.5,0))
+
+rank_sum = rank_quality %>%
+  rowSums()
+
+invest_quality = rank(rank_sum) <= 30
+
+KOR_ticker[invest_quality, ] %>%
+  select(`종목코드`, `종목명`) %>%
+  cbind(round(quality_profit[invest_quality, ], 4))
